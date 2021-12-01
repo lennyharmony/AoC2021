c0130444b2af1baef9cdb68e7ba9ded8b445fcbc
lennyharmony <37368960+lennyharmony@users.noreply.github.com>
2021-12-01 22:30:17 +0100

    create arraylist from txt file

--- 1/src/Main.java
+++ 1/src/Main.java
@@ -1,8 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        Scanner scanner = new Scanner(new File("input.txt"));
 
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(scanner.hasNextInt())
+        {
+            list.add(scanner.nextInt());
+        }
+    }
 }
 
 
