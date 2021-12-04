5b11cfff6baec28a16880ab7e15499dce9eb92bc
lennyharmony <37368960+lennyharmony@users.noreply.github.com>
2021-12-04 19:33:45 +0100

    read file and create callList

--- 4/src/Main.java
+++ 4/src/Main.java
@@ -1,4 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+        ArrayList<String> input = readFile();
+        ArrayList<Integer> numberCallList = getCallList(input);
+        
 
+
+
+    }
+
+    static ArrayList<String> readFile() {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("input.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add(scanner.nextLine());
+
+        while (true) {
+            assert scanner != null;
+            if (!scanner.hasNext()) break;
+            list.add(scanner.nextLine());
+        }
+        return list;
+    }
+
+    static ArrayList<Integer> getCallList(ArrayList<String> input) {
+        ArrayList<Integer> callList = new ArrayList<>();
+        String[] elements = input.get(0).split(",");
+
+        for (String element : elements) {
+            callList.add(Integer.parseInt(element.trim()));
+        }
+
+        return callList;
+    }
 }
